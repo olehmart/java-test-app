0aa7e301837de88661cdf5463afeff5cabf8ba1d
Oleh Martushevskyi <oleh_martushevskyi@epam.com>
2022-09-29 14:51:00 -0400

    feat: 3

--- .github/scripts/deployment_trigger/main.py
+++ .github/scripts/deployment_trigger/main.py
@@ -97,13 +97,8 @@ print("Cloning deployments repo to {} ...".format(clone_folder))
 DEPLOYMENTS_REPO = "https://{}:x-oauth-basic@github.com/{}/{}".format(GITHUB_TOKEN, deployment_git_project,
                                                                       deployment_git_repo_name)
 deployments_repo = Repo.clone_from(DEPLOYMENTS_REPO, "{}".format(clone_folder))
-#
-#
-# ########
-# # TEST #
-# ########
-# deployments_repo.git.config("--global", "user.email", "automation@example.com")
-# deployments_repo.git.config("--global", "user.name", "GitHub BOT")
+deployments_repo.git.config("--global", "user.email", "automation@example.com")
+deployments_repo.git.config("--global", "user.name", "GitHub BOT")
 
 
 #########################################
